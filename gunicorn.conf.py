a8b67e811e1abde0793c4bd14184d17b1a7af315
Gill Marry Bushinsky <ggbushi@Gills-MacBook-Air.local>
2025-08-07 23:33:12 +0300

    Remove unnecessary gunicorn.conf.py causing import error

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import logging
-from server import init_mongodb
-
-logger = logging.getLogger(__name__)
-
-def worker_init(worker):
-    logger.info(f"Initializing MongoDB for worker {worker.pid}")
-    init_mongodb()
